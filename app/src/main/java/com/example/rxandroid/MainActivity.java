867c36aa6c8f3b1d17ee6af0fd5b1389a8ad25db
huhuaijun1992 <1130456163@qq.com>
2018-03-13 13:52:10 +0800

    增加使用案例

--- app/src/main/java/com/example/rxandroid/MainActivity.java
+++ app/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,29 +1,52 @@
 package com.example.rxandroid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button button1,button2;
+    private Button button1,button2,button3,button4,button5;
+    private ImageView imageView;
+    private OkHttpClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showRxAndroid();
+        init();
     }
 
     public void  init(){
         button1=(Button)findViewById(R.id.first_button);
         button2=(Button)findViewById(R.id.two_button);
+        button3=(Button)findViewById(R.id.third_button);
+        button4=(Button)findViewById(R.id.four_button);
+        button5=(Button)findViewById(R.id.fiveth_button) ;
+        imageView=(ImageView)findViewById(R.id.local_image) ;
+
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        client=new OkHttpClient();
 
     }
     public void showRxAndroid(){
@@ -107,6 +130,86 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+    //加载一张本地图片
+    public void showLocalImage(){
+        final String imagPath="file:///android_asset/icon1.jpg";
+        Observable observable = Observable.just(imagPath);
+          observable.map(new Func1<String, Bitmap>() {
+            @Override
+            public Bitmap call(String s) {
+                Log.i("hhj", "call: ==="+s);
+                return BitmapFactory.decodeResource(getResources(),R.mipmap.icon1);
+            }
+        }).subscribeOn(Schedulers.newThread()) //开启一个新的线程
+                .observeOn(AndroidSchedulers.mainThread())
+                 .subscribe(new Subscriber<Bitmap>() {
+                     @Override
+                     public void onCompleted() {
+                         imageView.setVisibility(View.VISIBLE);
+                         Log.i("hhj", "onCompleted: 图片加载完成");
+                     }
+
+                     @Override
+                     public void onError(Throwable e) {
+
+                     }
+
+                     @Override
+                     public void onNext(Bitmap bitmap) {
+                         imageView.setImageBitmap(bitmap);
+
+                     }
+                 });
+    }
+
+    public void showOnlineImage(final String httpImagePath){
+        Observable.create(new Observable.OnSubscribe<byte[]>() {
+            @Override
+            public void call(final Subscriber<? super byte[]> subscriber) {
+                Request request=new Request.Builder().url(httpImagePath).build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        subscriber.onError(new Throwable("错误"));
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()){
+                            subscriber.onNext(response.body().bytes());
+
+                        }else {
+                            subscriber.onError(new Throwable("错误"));
+                        }
+
+                    }
+                });
+
+            }
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<byte[]>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(MainActivity.this,"加载失败",Toast.LENGTH_SHORT);
+
+                    }
+
+                    @Override
+                    public void onNext(byte[] bytes) {
+                        Bitmap bitmap=BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                        imageView.setImageBitmap(bitmap);
+                        imageView.setVisibility(View.VISIBLE);
+                    }
+                });
+
+    }
 
     @Override
     public void onClick(View view) {
@@ -119,6 +222,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.third_button:
                 filter();
+                break;
+            case R.id.four_button:
+                showLocalImage();
+                break;
+            case R.id.fiveth_button:
+                showOnlineImage("http://pic.58pic.com/58pic/15/91/28/76E58PICWqY_1024.jpg");
                 break;
 
         }
